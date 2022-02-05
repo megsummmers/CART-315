8b34b70e180e0c1e230f190f1fcee9e551c457ca
megsummmers <70597858+megsummmers@users.noreply.github.com>
2022-02-04 20:15:38 -0500

    Update: Star animation
    added an animation to the stars so they move when collected and *hopefully* when all are collected

--- 315 Prototype/Assets/activateStar.cs
+++ 315 Prototype/Assets/activateStar.cs
@@ -4,32 +4,49 @@ using UnityEngine;
 
 public class activateStar : MonoBehaviour
 {
-  int posY;
   bool collected = false;
+  bool endScene = false;
+  int starCount = 0;
 
   void Start()
   {
-    posY = 4;
+
   }
 
   void FixedUpdate()
   {
-    // if(collected){
-    //   transform.Translate(0, posY, 0)
-    // }
+    if (collected == true)
+    {
+      transform.Translate(0, Time.deltaTime, 0, Space.World);
+      float posY = gameObject.transform.position.y;
+      if (posY >= 5.5){
+        collected = false;
+        starCount++;
+      }
+    }
+
+    if(starCount == 10 && !endScene)
+    {
+      transform.Translate(0, Time.deltaTime, 0, Space.World);
+      float posY = gameObject.transform.position.y;
+      if (posY >= 15){
+        endScene = true;
+      }
+    }
   }
 
   void OnTriggerEnter(Collider other)
   {
     if(other.CompareTag("Player")){
       Debug.Log("Picked up star");
-      collected = true;
       //activate Halo around star
       Component halo = GetComponent("Halo");
       halo.GetType().GetProperty("enabled").SetValue(halo, true, null);
       //enable light to help guide the user
       Component light = GetComponent("Light");
       light.GetType().GetProperty("enabled").SetValue(light, true, null);
+      //raise the star
+      collected = true;
     }
   }
 }
